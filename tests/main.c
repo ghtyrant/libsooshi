43c924fabdcd8fa3666be029bd710453e9728d9c
Fabian Schlager <fabian.schlager@skyr.at>
2016-06-19 18:53:51 +0200

    Updated test to reflect latest changes

--- tests/main.c
+++ tests/main.c
@@ -2,31 +2,35 @@
 
 #include "sooshi.h"
 
-void battery_update(SooshiState *state, SooshiNode *node)
+void battery_update(SooshiState *state, SooshiNode *node, gpointer user_data)
 {
     float voltage = g_variant_get_double(node->value);
     printf("Battery Percent: %.2f\n", (voltage - 2.0) * 100.0);
 }
 
-void channel1_update(SooshiState *state, SooshiNode *node)
+void channel1_update(SooshiState *state, SooshiNode *node, gpointer user_data)
 {
     //printf("Channel 1: %.2f\n", g_variant_get_double(node->value));
 }
 
-void channel2_update(SooshiState *state, SooshiNode *node)
+void channel2_update(SooshiState *state, SooshiNode *node, gpointer user_data)
 {
     printf("Channel 2: %f\n", g_variant_get_double(node->value));
+    sooshi_stop(state);
+    sooshi_state_delete(state);
 }
 
 
-void mooshi_initialized(SooshiState *state)
+void mooshi_initialized(SooshiState *state, gpointer user_data)
 {
-    sooshi_node_subscribe(state, sooshi_node_find(state, "BAT_V", NULL), battery_update);
-    sooshi_node_subscribe(state, sooshi_node_find(state, "CH1:VALUE", NULL), channel1_update);
-    sooshi_node_subscribe(state, sooshi_node_find(state, "CH2:VALUE", NULL), channel2_update);
+    g_info("Mooshimeter initialized, starting subscribers!");
+
+    sooshi_node_subscribe(state, sooshi_node_find(state, "BAT_V", NULL), battery_update, NULL);
+    sooshi_node_subscribe(state, sooshi_node_find(state, "CH1:VALUE", NULL), channel1_update, NULL);
+    sooshi_node_subscribe(state, sooshi_node_find(state, "CH2:VALUE", NULL), channel2_update, NULL);
 
     sooshi_node_choose(state, sooshi_node_find(state, "SAMPLING:TRIGGER:CONTINUOUS", NULL));
-    sooshi_node_choose(state, sooshi_node_find(state, "CH2:MAPPING:VOLTAGE:60", NULL));
+    //sooshi_node_choose(state, sooshi_node_find(state, "CH2:MAPPING:TEMP:350", NULL));
 }
 
 int main()
@@ -35,8 +39,8 @@ int main()
 
     SooshiState *state;
     state = sooshi_state_new();
-    sooshi_run(state, mooshi_initialized);
-    sooshi_state_delete(state);
+    sooshi_setup(state, mooshi_initialized, NULL);
+    sooshi_run(state);
 
     return 0;
 }
